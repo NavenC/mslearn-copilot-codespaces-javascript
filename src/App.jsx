82ceef55b008cc5056236cf070cfbf5c6ec3737a
NavenC <147365327+NavenC@users.noreply.github.com>
2025-08-26 04:57:26 +0000

    Update siteProps with new user information and enhance social icon animations

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Naveen Gaddam",
+  title: "AWS Cloud Engineer | To Be DevOps Engineer & Kubernetes Admin",
+  email: "naven.gaddam@gmail.com",
+  gitHub: "BitBucket",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "navengaddam",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
